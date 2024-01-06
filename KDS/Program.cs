2b838c8dac99e157031d97e63330bf34079ffaa6
Plerx <50530305+Plerx2493@users.noreply.github.com>
2024-01-07 00:56:57 +0100

    first commit

---
+++ KDS/Program.cs
@@ -0,0 +1,92 @@
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using KDS.Components;
+using KDS.Components.Account;
+using KDS.Data;
+
+namespace KDS;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        builder.Services.AddCascadingAuthenticationState();
+        builder.Services.AddScoped<IdentityUserAccessor>();
+        builder.Services.AddScoped<IdentityRedirectManager>();
+        builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+        builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            })
+            .AddTwitch(config =>
+            {
+                config.UsePkce = true;
+                config.SaveTokens = true;
+
+                config.ClientId = builder.Configuration["Authentication:Twitch:ClientId"] ??
+                                  throw new InvalidOperationException("Twitch ClientId not found.");
+                
+                config.ClientSecret = builder.Configuration["Authentication:Twitch:ClientSecret"] ?? 
+                                      throw new InvalidOperationException("Twitch ClientSecret not found.");
+                
+                config.Events.OnCreatingTicket +=  (context) =>
+                {
+                    var services = context.HttpContext.RequestServices; 
+                    //var tokenRepo = services.GetRequiredService<ITokenRepository>();
+                    //tokenRepo.SetToken(context);
+
+                    return Task.CompletedTask;
+                };
+            })
+            .AddIdentityCookies();
+
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                               throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(connectionString));
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+        builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddSignInManager()
+            .AddDefaultTokenProviders();
+
+        builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseMigrationsEndPoint();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        // Add additional endpoints required by the Identity /Account Razor components.
+        app.MapAdditionalIdentityEndpoints();
+
+        app.Run();
+    }
+}
